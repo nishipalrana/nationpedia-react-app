a0cd99d4086255c0055e4d7e587c15759cd4f7a9
Nishipal Rana <nishipalrana@outlook.com>
2022-06-19 17:04:54 +0530

    Added new Endpoints to service

--- src/services/NationpediaService.js
+++ src/services/NationpediaService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const service = {};
 
 service.getCountriesList = async () => {
-  return axios.get("https://restcountries.com/v2/all?fields=name,capital,flags");
+  return axios.get("https://restcountries.com/v3.1/all?fields=name,capital,flags");
 };
 
 service.getCountryNameList = async () => {
@@ -16,4 +16,12 @@ service.getCountryInfo = async (name) => {
 
 service.getCountriesByRegion = async (region) => {
   return axios.get(`https://restcountries.com/v3.1/region/${region}`);
+};
+
+service.getCurrencyList = async () => {
+  return axios.get("https://restcountries.com/v3.1/all?fields=currencies");
+};
+
+service.getCountriesByCurrency =async (currency) => {
+  return axios.get(`https://restcountries.com/v3.1/currency/${currency}?fields=name,capital,flags`);
 };
